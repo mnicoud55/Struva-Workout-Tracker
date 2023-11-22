2e467b2ddadce8850d68d96ed125c0003503f6a1
Justin Brady <jmb4zz@virginia.edu>
2023-11-22 16:07:03 -0500

    adding url to comment on main_page

--- main_page.php
+++ main_page.php
@@ -1,5 +1,10 @@
 <?php
+//main page URL
+//https://www.cs.virginia.edu/~jmb4zz/CS4750_final_deliverable/simpleform.php
+
 require("connect-db.php");
 require("workout-db.php");
 
 echo "Hello World" ?>
+
+
